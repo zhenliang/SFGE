4a899eff0e4a015beac8171fb66e91832c6b58df
zhenliang <home.zhenliang@gmail.com>
2011-05-30 00:15:04 +0800

    Improved Logger class.

--- util/logger.cpp
+++ util/logger.cpp
@@ -2,10 +2,40 @@
 
 #include <iostream>
 #include <string>
+#include <ctime>
+#include <iomanip>
 
 namespace
 {
   const std::string kDefaultFile = "log.txt";
+
+  void PrintTime(std::ostream& os)
+  {
+    time_t rawtime;
+    time(&rawtime);
+    tm* timeinfo = localtime(&rawtime);
+    os << std::setfill('0');
+    os << "[" 
+      << std::setw(4) << timeinfo->tm_year + 1900 << "-"
+      << std::setw(2) << timeinfo->tm_mon + 1<< "-"
+      << std::setw(2) << timeinfo->tm_mday << " "
+      << std::setw(2) << timeinfo->tm_hour << ":"
+      << std::setw(2) << timeinfo->tm_min << ":"
+      << std::setw(2) << timeinfo->tm_sec << "] ";
+    os << std::setfill(' ');
+  }
+
+  void PrintLoglevel(std::ostream& os, int loglevel)
+  {
+    switch (loglevel)
+    {
+    case util::Logger::LL_INFO: os << "[INFO] "; break;
+    case util::Logger::LL_WARNING: os << "[WARNING] "; break;
+    case util::Logger::LL_ERROR: os << "[ERROR] "; break;
+    case util::Logger::LL_NONE: os << "[NONE] "; break;
+    default: os << "[UNKNOWN] "; break;
+    }
+  }
 }
 
 namespace util
@@ -27,6 +57,9 @@ namespace util
 
     if (!outstream_.is_open())
       outstream_.open(logfile_.c_str());
+
+    PrintTime(outstream_);
+    PrintLoglevel(outstream_, loglevel);
 
     if ((logMode_ & LM_FILE) && outstream_.is_open() && outstream_.good())
       outstream_ << text << std::endl;
